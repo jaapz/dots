ed759377c82084a2af5444696e458becc65a7b39
Jaap Broekhuizen <jaapz.b@gmail.com>
2016-04-20 10:47:50 +0200

    Update mbo function to new way of specifying environments

--- home/.zsh/mbo.sh
+++ home/.zsh/mbo.sh
@@ -1,12 +1,5 @@
 function mbo() {
-    if [ -n $1 ]; then
-        ENV=$1
-    else
-        ENV='prod'
-    fi
-
     cd ~/programming/mijnbaopt
-    export MIJNBAOPT_ENV=$ENV
     export MIJNBAOPT_DIR='/home/jaapz/programming/mijnbaopt'
     export MIJNBAOPT_VENV='/home/jaapz/programming/mijnbaopt/.env'
     activate
